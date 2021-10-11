4e6d2bd814895ff03ef6f55b6d9c21bb89975b53
zainab-ali <zainab.ali@intenthq.com>
2021-10-11 16:02:27 +0100

    Solutions to Algebraic Data Types exercise

--- src/main/scala/pie/PizzaShop.scala
+++ src/main/scala/pie/PizzaShop.scala
@@ -27,13 +27,42 @@ object PizzaShop {
   def parseSize(size: String): Either[NotASize.type, Int] =
     size.toIntOption.toRight(NotASize).map(_ * 10)
 
-  def validateSize(size: Int): Either[PizzaError, Pizza] = ???
+  def validateSize(size: Int): Either[PizzaError, Pizza] =
+    if (size < 0) Left(NegativeSize)
+    else if (size < 3) Left(PizzaTooSmall)
+    else if (size > 16) Left(PizzaTooBig)
+    else Right(Pizza(size, Tomato))
 
-  def correction(error: PizzaError): Either[PizzaError, Pizza] = ???
+  def correction(error: PizzaError): Either[PizzaError, Pizza] = error match {
+    case PizzaTooSmall => Right(Pizza(3, Tomato))
+    case PizzaTooBig => Right(Pizza(16, Tomato))
+    case other => Left(other)
+  }
 
-  def validateSauce(sauce: String): Either[PizzaError, Sauce] = ???
+  def validateSauce(sauce: String): Either[PizzaError, Sauce] =
+    if (sauce == "white") Right(Bechamel)
+    else if (sauce == "red") Right(Tomato)
+    else Left(StrangeSauce)
 
-  def validatePizza(size: Int, sauce: String): Either[NonEmptyList[PizzaError], Pizza] = ???
+  // This solution only uses pattern matching - it doesn't use a cats.data.Validated or any functions for handling Either
+  // Try playing around with the following code:
+  // > import cats.implicits._
+  // > validateSize(size).handleErrorWith(correction).toValidatedNel
+  def validatePizza(size: Int, sauce: String): Either[NonEmptyList[PizzaError], Pizza] = {
+    val eitherSizeOrError = validateSize(size) match {
+      case Right(size) => Right(size)
+      case Left(error) => correction(error)
+    }
+    val eitherSauceOrError = validateSauce(sauce)
+    (eitherSizeOrError, eitherSauceOrError) match {
+      case (Left(sizeError), Left(sauceError)) => Left(NonEmptyList(sizeError, List(sauceError)))
+      case (Left(sizeError), Right(_)) => Left(NonEmptyList(sizeError, Nil))
+      case (Right(_), Left(sauceError)) => Left(NonEmptyList(sauceError, Nil))
+      case (Right(pizza), Right(sauce)) => Right(pizza.copy(size = size, sauce = sauce))
+    }
+  }
+
+
 
   def main(args: Array[String]): Unit = {
     val pizzaSize = args(0).toInt * 10
