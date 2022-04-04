dc9c2c1e47487937a3531927a0168050f95ee4c3
zainab-ali <zainab.ali@intenthq.com>
2022-04-04 11:22:12 +0100

    Add code changes

--- src/main/scala/pie/core/Sauce.scala
+++ src/main/scala/pie/core/Sauce.scala
@@ -16,9 +16,7 @@ import doodle.image.*
 //     def toImage(size: Int): Image = Image.circle(size * 0.75).fillColor(sauceColor).noStroke
 //   }
 
-
 //   override def toImage(size: Int): Image =  Sauce.toImage[Tomato.type](size)(sauceToImage)
-
 
 //   val numTomatoes: Int = 23
 // }
@@ -31,26 +29,30 @@ import doodle.image.*
 object Sauce {
 
   // def toImage(size: Int, sauce: Sauce): Image = sauce.toImage(size)
-  def toImage[A](size: Int)(implicit sauceToImage: SauceToImage[A]): Image = sauceToImage.toImage(size)
+  def toImage[A](size: Int)(implicit sauceToImage: SauceToImage[A]): Image =
+    sauceToImage.toImage(size)
 //  def toImage[A, S <: SauceToImage[A]](size: Int)(implicit sauceToImage: S): Image = sauceToImage.toImage(size)
 
 }
 
 final class FixedColorSauceToImage[A](color: Color) extends SauceToImage[A] {
-  def toImage(size: Int): Image = Image.circle(size * 0.75).fillColor(color).noStroke
+  def toImage(size: Int): Image =
+    Image.circle(size * 0.75).fillColor(color).noStroke
 }
 
 sealed trait CoreSauce
 object Tomato2 extends CoreSauce {
 
-  implicit val sauceToImage: FixedColorSauceToImage[Tomato2.type] = new FixedColorSauceToImage[Tomato2.type](Color.red)
+  implicit val sauceToImage: FixedColorSauceToImage[Tomato2.type] =
+    new FixedColorSauceToImage[Tomato2.type](Color.red)
 //    new SauceToImage[Tomato2.type] {
 //    def toImage(size: Int): Image = Image.circle(size * 0.75).fillColor(Color.red).noStroke
 //  }
 
 }
 object Bechamel2 extends CoreSauce {
-  implicit val sauceToImage: FixedColorSauceToImage[Bechamel2.type] = new FixedColorSauceToImage[Bechamel2.type](Color.white)
+  implicit val sauceToImage: FixedColorSauceToImage[Bechamel2.type] =
+    new FixedColorSauceToImage[Bechamel2.type](Color.white)
 //    new SauceToImage[Bechamel2.type] {
 //    override def toImage(size: Int): Image = Image.circle(size * 0.75).fillColor(Color.white).noStroke
 //  }
--- src/main/scala/pie/core/SauceToImage.scala
+++ src/main/scala/pie/core/SauceToImage.scala
@@ -2,9 +2,11 @@ package pie.core
 
 import doodle.image.Image
 
+// Sauce
+//  Colour - Property
+// Image
+// Size - Property
 trait SauceToImage[A] {
-  def toImage(size: Int): Image
+  def toImage(sauce: A, size: Int): Image
 
 }
-
-
